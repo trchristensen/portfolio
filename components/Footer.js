cf3912b2a907f1cdf7252490a8768ba0abd428bc
Todd Christensen <christensen.tr@gmail.com>
2020-03-18 13:32:05 +0800

    making components

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+export default () => {
+  return (
+    <footer class="bg-gray-900">
+      <div class="container mx-auto">
+        <div class="row">
+          <div class="col p-4">
+            <p class="text-base text-gray-600 text-center">
+              Site made with ☕ ..and 🍺
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,14 @@
+import Sidenav from "../components/Sidenav";
+import Footer from "../components/Footer";
+
 export default ({ children }) => {
   return (
-    <div className="antialiased text-gray-900 justify-center min-h-screen">
-      { children }
+    <div className="antialiased text-gray-900 justify-center min-h-screen" id="app">
+      <Sidenav />
+      <main>
+        { children }
+      </main>
+      <Footer />
     </div>
   )
 }
